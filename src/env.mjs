1467f6b1036477983f3e9326b509c79e7ebc2183
zubayerdev <zubayerdev.org@gmail.com>
2025-12-10 02:43:48 +0600

    new

--- src/env.mjs
+++ src/env.mjs
@@ -3,11 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    SITE_URL: z.string().url(),
-    RESEND_API_KEY: z.string().min(1),
+    SITE_URL: z.string().url().default("http://localhost:3000"),
+    RESEND_API_KEY: z.string().min(1).default("test_api_key"),
   },
   client: {
-    NEXT_PUBLIC_SITE_URL: z.string().url(),
+    NEXT_PUBLIC_SITE_URL: z.string().url().default("http://localhost:3000"),
   },
   runtimeEnv: {
     SITE_URL: process.env.SITE_URL,
